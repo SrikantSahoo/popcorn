8168a43f52a9eb3b2732d1961c904c4f58cba720
Srikanta Sahoo <ss0049355@gmail.com>
2015-05-14 03:02:45 +1000

    added youtube api

---
+++ app/assets/javascripts/app/controllers/movies.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('popcornApp.controllers')
+	.controller('MoviesController', function($scope,MoviesService){
+
+			MoviesService.movies().then(function(movies) {
+			  $scope.movies = movies; 
+			});
+
+			$scope.addFavorite = function(movie) {
+				  movie.isFavorite = true;
+			}; 
+
+			$scope.removeFavorite = function(movie) {
+				  movie.isFavorite = false;
+			};
+
+		});
